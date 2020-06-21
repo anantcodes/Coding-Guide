1ac0a106c69b62492a2921823256492427d338d8
Anant Kanchan <anant4265@gmail.com>
2020-06-21 17:57:55 +0530

    initial commit message

---
+++ app/src/main/java/com/anant/codingguide/ControlActivity.kt
@@ -0,0 +1,11 @@
+package com.anant.codingguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ControlActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_control)
+    }
+}
--- app/src/main/java/com/anant/codingguide/WelcomeActivity.kt
+++ app/src/main/java/com/anant/codingguide/WelcomeActivity.kt
@@ -1,11 +1,26 @@
 package com.anant.codingguide
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
+
+
+    lateinit var btnNext:Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        btnNext=findViewById(R.id.btnNext)
+
+        btnNext.setOnClickListener {
+            startActivity(Intent(this@WelcomeActivity, ControlActivity::class.java))
+        }
+
+
     }
 }
