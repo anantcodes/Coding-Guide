dc0ff6a787182f963c07553aca51b810a749e6a2
Anant Kanchan <anant4265@gmail.com>
2020-06-23 17:41:28 +0530

    initial commit message

--- app/src/main/java/com/anant/codingguide/ControlActivity.kt
+++ app/src/main/java/com/anant/codingguide/ControlActivity.kt
@@ -2,10 +2,35 @@ package com.anant.codingguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
+import android.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class ControlActivity : AppCompatActivity() {
+
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var coordinatorLayout: CoordinatorLayout
+    lateinit var toolbar: Toolbar
+    lateinit var frameLayout: FrameLayout
+    lateinit var navigationView: NavigationView
+
+    var previousMenuItem: MenuItem?=null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_control)
+
+        drawerLayout=findViewById(R.id.drawerLayout)
+        coordinatorLayout=findViewById(R.id.coordinatorLayout)
+        toolbar=findViewById(R.id.toolbar)
+        frameLayout=findViewById(R.id.frame)
+        navigationView=findViewById(R.id.navigationView)
+
+
     }
 }
