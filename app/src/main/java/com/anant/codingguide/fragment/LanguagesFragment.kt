889b5a5ee266f4ae1a740f5cf20702e9b6cc6983
Anant Kanchan <anant4265@gmail.com>
2020-06-24 11:56:44 +0530

    initial commit message

--- app/src/main/java/com/anant/codingguide/fragment/LanguagesFragment.kt
+++ app/src/main/java/com/anant/codingguide/fragment/LanguagesFragment.kt
@@ -1,22 +1,45 @@
 package com.anant.codingguide.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.anant.codingguide.R
+import kotlinx.android.synthetic.main.fragment_languages.*
 
 
 class LanguagesFragment : Fragment() {
 
+   lateinit var btnJava:Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_languages, container, false)
+        val view=inflater.inflate(R.layout.fragment_languages, container, false)
+
+       btnJava=view.findViewById(R.id.btnJava)
+
+        btnJava.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.geeksforgeeks.org/java-tutorials/?ref=lbp"))
+            activity?.startActivity(i)
+
+        }
+
+
+
+
+
+        return view
+
+
+
+
     }
 
 
