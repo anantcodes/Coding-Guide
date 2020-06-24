d8f6a242b770a889a924c03d3cd84e521ab45908
Anant Kanchan <anant4265@gmail.com>
2020-06-24 18:46:20 +0530

    initial commit message

--- app/src/main/java/com/anant/codingguide/fragment/CompilerFragment.kt
+++ app/src/main/java/com/anant/codingguide/fragment/CompilerFragment.kt
@@ -1,21 +1,36 @@
 package com.anant.codingguide.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.anant.codingguide.R
 
 
 class CompilerFragment : Fragment() {
+
+
+    lateinit var btnIde:Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compiler, container, false)
+        val view=inflater.inflate(R.layout.fragment_compiler, container, false)
+
+        btnIde=view.findViewById(R.id.btnIde)
+
+        btnIde.setOnClickListener {
+            activity?.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://ideone.com")))
+
+        }
+
+        return view
     }
 
 
