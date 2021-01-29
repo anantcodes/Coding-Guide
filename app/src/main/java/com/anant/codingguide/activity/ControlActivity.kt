cb72472efc015e28bd4b76db612630da0832bae6
Amin <dadgaramin96@gmail.com>
2021-01-29 19:17:41 +0330

    new animations added for fragments

--- app/src/main/java/com/anant/codingguide/activity/ControlActivity.kt
+++ app/src/main/java/com/anant/codingguide/activity/ControlActivity.kt
@@ -16,6 +16,7 @@ import com.anant.codingguide.R
 import com.anant.codingguide.fragment.*
 import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AlertDialog.Builder
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 
 
@@ -27,7 +28,7 @@ class ControlActivity : AppCompatActivity() {
     lateinit var toolbar: Toolbar
     lateinit var frameLayout: FrameLayout
     lateinit var navigationView: NavigationView
-    var previousMenuItem:MenuItem?=null
+    var previousMenuItem: MenuItem? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,77 +42,65 @@ class ControlActivity : AppCompatActivity() {
         navigationView = findViewById(R.id.navigationView)
 
         openHome()
-        supportActionBar?.title="Home"
+        supportActionBar?.title = "Home"
 
 
         setUpToolbar()
 
-        val actionBarDrawerToggle=ActionBarDrawerToggle(this@ControlActivity,drawerLayout,R.string.open_drawer,R.string.close_drawer)
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this@ControlActivity,
+            drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
 
         navigationView.setNavigationItemSelectedListener {
 
 
-            if(previousMenuItem != null){
-                previousMenuItem?.isChecked=false
+            if (previousMenuItem != null) {
+                previousMenuItem?.isChecked = false
             }
-            it.isCheckable=true
-            it.isChecked=true
-            previousMenuItem=it
+            it.isCheckable = true
+            it.isChecked = true
+            previousMenuItem = it
 
-            when(it.itemId)
-            {
-                R.id.home->{
-                      openHome()
+
+            when (it.itemId) {
+                R.id.home -> {
+                    openHome()
                     drawerLayout.closeDrawers()
 
                 }
 
-                R.id.language->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,
-                        LanguagesFragment()
-                    )
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                        .commit()
-                    supportActionBar?.title="Programming languages"
+                R.id.language -> {
+                    setUpFragment(LanguagesFragment())
+                    supportActionBar?.title = "Programming languages"
                     drawerLayout.closeDrawers()
                 }
 
-                R.id.dev->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,
-                        DevelopmentFragment()
-                    )
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                        .commit()
-                    supportActionBar?.title="Software Development"
+                R.id.dev -> {
+                    setUpFragment(DevelopmentFragment())
+                    supportActionBar?.title = "Software Development"
                     drawerLayout.closeDrawers()
                 }
 
-                R.id.quest->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,
-                        CompetitiveProgrammingFragment()
-                    )
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                        .commit()
-                    supportActionBar?.title="Competitive programming"
+                R.id.quest -> {
+                    setUpFragment(CompetitiveProgrammingFragment())
+                    supportActionBar?.title = "Competitive programming"
                     drawerLayout.closeDrawers()
                 }
 
 
-
-                R.id.compiler->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,
-                        CompilerFragment()
-                    )
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                        .commit()
-                    supportActionBar?.title="Test your Code"
+                R.id.compiler -> {
+                    setUpFragment(CompilerFragment())
+                    supportActionBar?.title = "Test your Code"
                     drawerLayout.closeDrawers()
                 }
 
 
-                R.id.exit->{
+                R.id.exit -> {
                     val builder: android.app.AlertDialog.Builder =
                         android.app.AlertDialog.Builder(this@ControlActivity)
                     builder.setTitle(R.string.app_name)
@@ -127,7 +116,6 @@ class ControlActivity : AppCompatActivity() {
                 }
 
 
-
             }
 
 
@@ -140,43 +128,52 @@ class ControlActivity : AppCompatActivity() {
 
     }
 
-    fun setUpToolbar(){
+    fun setUpToolbar() {
         setSupportActionBar(toolbar)
-            //supportActionBar?.title="Toolbar Title"
+        //supportActionBar?.title="Toolbar Title"
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    fun setUpFragment(fragment: Fragment){
+        val manager = supportFragmentManager.beginTransaction()
+        manager.setCustomAnimations(
+            R.anim.slide_in,
+            R.anim.slide_out)
+
+        manager.replace(
+            R.id.frame,
+            fragment
+        )
+        manager.commit()
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id=item.itemId
-        if(id==android.R.id.home)
-        {
+        val id = item.itemId
+        if (id == android.R.id.home) {
             drawerLayout.openDrawer(GravityCompat.START)
         }
         return super.onOptionsItemSelected(item)
     }
 
-   fun  openHome()
-    {
-        val fragment=HomeFragment()
-        val transaction=supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frame,fragment)
+    fun openHome() {
+        val fragment = HomeFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame, fragment)
         transaction.commit()
-        supportActionBar?.title="Home"
+        supportActionBar?.title = "Home"
         navigationView.setCheckedItem(R.id.home)
     }
 
     override fun onBackPressed() {
         val frag = supportFragmentManager.findFragmentById(R.id.frame)
 
-        when(frag){
+        when (frag) {
             !is HomeFragment -> openHome()
 
             else -> super.onBackPressed()
         }
     }
-
-
 
 
 }
